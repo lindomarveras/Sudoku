924c13da859461d03f643ffa7b772a160482666f
Cristina Pérez <pem.cristina@gmail.com>
2017-02-11 18:30:27 +0100

    Added a KeyboardFragment java and xml. Changed dimens.xml and strings.xml

---
+++ app/src/main/java/com/android/ejemplos/sudoku/KeyboardFragment.java
@@ -0,0 +1,20 @@
+package com.android.ejemplos.sudoku;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KeyboardFragment extends Fragment {
+
+
+    public KeyboardFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_keyboard, container, false);
+    }
+}
