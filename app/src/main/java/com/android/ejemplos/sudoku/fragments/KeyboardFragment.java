91e24f5cbf5a4e64450f81dbfc2f9787b9df5f17
Cristina Pérez <pem.cristina@gmail.com>
2017-02-12 18:15:27 +0100

    Deleted unused import in KeyBoardFragment

--- app/src/main/java/com/android/ejemplos/sudoku/fragments/KeyboardFragment.java
+++ app/src/main/java/com/android/ejemplos/sudoku/fragments/KeyboardFragment.java
@@ -2,13 +2,10 @@ package com.android.ejemplos.sudoku.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.android.ejemplos.sudoku.R;
 
