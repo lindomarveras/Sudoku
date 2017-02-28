23ab28ddc9ff81dc6befb1d08b17fe5f6b1e7ba2
Cristina Pérez <pem.cristina@gmail.com>
2017-02-28 18:16:37 +0100

    Changed number of cells according to level

--- app/src/main/java/com/android/ejemplos/sudoku/util/Constants.java
+++ app/src/main/java/com/android/ejemplos/sudoku/util/Constants.java
@@ -1,9 +1,9 @@
 package com.android.ejemplos.sudoku.util;
 
 public class Constants {
-    public static final int EASY_LEVEL_CELL_NUMBER = 90;
-    public static final int MEDIUM_LEVEL_CELL_NUMBER = 20;
-    public static final int HARD_LEVEL_CELL_NUMBER = 10;
+    public static final int EASY_LEVEL_CELL_NUMBER = 80;
+    public static final int MEDIUM_LEVEL_CELL_NUMBER = 45;
+    public static final int HARD_LEVEL_CELL_NUMBER = 30;
 
     public static final String EASY_LEVEL_TEXT = "Level: easy";
     public static final String MEDIUM_LEVEL_TEXT = "Level: medium";
